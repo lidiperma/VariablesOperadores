b339f1d5fd32625e8d51e524b27a79a9ea140abb
lidiperma <lidiperma@gmail.com>
2017-04-11 13:36:34 +0200

    Es para mi repositorio local

--- src/EjemplosOperadores/OperadoresAritmeticos.java
+++ src/EjemplosOperadores/OperadoresAritmeticos.java
@@ -12,17 +12,14 @@ public class OperadoresAritmeticos {
      int c = 25;
      int d = 25;
      
+     //OPERADORES ARITMÉTICOS 
      System.out.println("a + b = " + (a + b) );
      System.out.println("a - b = " + (a - b) );
      System.out.println("a * b = " + (a * b) );
      System.out.println("b / a = " + (b / a) );
      System.out.println("b % a = " + (b % a) );
      System.out.println("c % a = " + (c % a) );
-     System.out.println("a++   = " +  (a++) );
-     System.out.println("b--   = " +  (b--) );
-     // Mira la diferencia entre d++ and ++d
-     System.out.println("d++   = " +  (d++) );
-     System.out.println("++d   = " +  (++d) );
+     
      
     }
     
---
+++ src/EjemplosOperadores/OperadoresAsignacion.java
@@ -0,0 +1,23 @@
+
+package EjemplosOperadores;
+
+
+public class OperadoresAsignacion {
+
+   
+    public static void main(String[] args) {
+        
+        int x = 5, y = 5;
+        int z;
+        
+        //OPERADOR INCREMENTO
+        System.out.println("z = " + (z=x++));// z vale 5, x vale 6 >>>> z=x ; (x=x+1)
+        System.out.println("z = " + (z=++y));// z vale 6, y vale 6 >>>> (y=y+1) ; z=y
+        
+        //OPERADOR DECREMENTO
+        System.out.println("z = " + (z=x--));// z vale 5, x vale 4 >>>>> z=y ; (y=y-1)
+        System.out.println("z = " + (z=--y));// z vale 4, y vale 4 >>>>> (y=y-1) ; z=y
+        
+    }
+    
+}
